120f3006bc4e419a232a4f01775a2ba45fbdb413
JeremiahO89 <technologyfrog89@gmail.com>
2024-10-11 10:31:49 -0400

    first try

---
+++ Nuerl Network/main.py
@@ -0,0 +1,18 @@
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+train_data = datasets.MNIST (
+    root = 'data',
+    train = True,
+    transform = ToTensor(),
+    download = True
+    )
+
+test_data = datasets.MNIST (
+    root = 'data',
+    train = False,
+    transform = ToTensor(),
+    download = True
+    )
+
+print (train_data)
